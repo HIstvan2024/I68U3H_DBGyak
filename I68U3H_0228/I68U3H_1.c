29579a7bf43b54f73d4af878304188b1a6c8ce08
HIstvan2024 <hi0905@duck.com>
2025-03-22 21:28:29 +0100

    modify 1. task (1. konz)
    Frissítettem az első feladat forráskódját az átláthatóság érdekében és feltöltöttem a kód által generált .txt fájlt is.

--- I68U3H_0228/I68U3H_1.c
+++ I68U3H_0228/I68U3H_1.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int main(int argc, char **argv)
+void betuValto(char a[], char b[], char c[])
 {
 	int i = 0;
-	char nevEsNeptun[32], kisBetu[32], nagyBetu[32];
-	
-    FILE* fp;
-    fp = fopen("neptunkod.txt", "w");
-    fgets(nevEsNeptun, 32, stdin);
-    fprintf(fp, "%s", nevEsNeptun);
+	FILE* fp;
+    fp = fopen("i68u3h.txt", "w");
+    fgets(a, 32, stdin);
+    fprintf(fp, "%s", a);
     fclose(fp);
 
-    fp = fopen("neptunkod.txt", "r");
-    fgets(kisBetu, 32, fp);
+    fp = fopen("i68u3h.txt", "r");
+    fgets(b, 32, fp);
     fclose(fp);
     
-    for(i = 0; i < 32; i++) nagyBetu[i] = toupper(kisBetu[i]);
+    for(i = 0; i < 32; i++) c[i] = toupper(b[i]);
     
-    fp = fopen("neptunkod.txt", "w");
-    fprintf(fp, "%s", nagyBetu);
+    fp = fopen("i68u3h.txt", "w");
+    fprintf(fp, "%s", c);
     fclose(fp);
-    
+}
+	
+
+int main(int argc, char **argv)
+{
+	char nevEsNeptun[32], bejovo[32], kimeno[32];
+	betuValto(nevEsNeptun, bejovo, kimeno);
 	return 0;
 }
